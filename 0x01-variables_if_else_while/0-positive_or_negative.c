e650564e0b7dfdb09f5f221a819bcffc437ac56e
Wright <calebogundiya@gmail.com>
2023-03-19 22:35:58 +0000

    Random number generator

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,13 +15,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%u is positive", n);
+		printf("%u is positive\n", n);
 	} else if (n < 0)
 	{
-		printf("%d is negative", n);
+		printf("%d is negative\n", n);
 	} else
 	{
-		printf("%u is zero", n);
+		printf("%u is zero\n", n);
 	}
 	return (0);
 }
