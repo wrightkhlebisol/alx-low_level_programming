1ee3487004353bb721f1c1fd562794f657630b3a
Wright <calebogundiya@gmail.com>
2023-03-19 19:03:20 +0000

    Print escaped character

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - start the program
+*
+* Return: 1
+*/
+
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
