193748c77b70b972a889612d9da5fc706db8d717
Wright <calebogundiya@gmail.com>
2023-03-19 19:10:55 +0000

    Intel syntax

---
+++ 0x00-hello_world/4-puts.s
@@ -0,0 +1,28 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 13, 0	sdk_version 13, 1
+	.intel_syntax noprefix
+	.globl	_main                           ## -- Begin function main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## %bb.0:
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset rbp, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register rbp
+	sub	rsp, 16
+	mov	dword ptr [rbp - 4], 0
+	lea	rdi, [rip + L_.str]
+	call	_puts
+	xor	eax, eax
+	add	rsp, 16
+	pop	rbp
+	ret
+	.cfi_endproc
+                                        ## -- End function
+	.section	__TEXT,__cstring,cstring_literals
+L_.str:                                 ## @.str
+	.asciz	"\"Programming is like building a multilingual puzzle"
+
+.subsections_via_symbols
