0fc15c7350b6461bd48e5aa0fec72cee20819380
Wright <calebogundiya@gmail.com>
2023-03-20 23:06:06 +0000

    Numberz putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int x;
 
-	for (x = 0; x <= 9; x++)
+	for (x = '0'; x <= '9'; x++)
 	{
 		putchar(x);
 	}
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
+#include <ctype.h>
 
 /**
 * main - start the app
 *
-* Return: 1
+* Return: 0
 */
 
 int main(void)
 {
-	
-	return (1);
+	int x;
+
+	for (x = '0x0'; x <= '0x0'; x++)
+	{
+		printf("%d", x);
+	}
+	putchar('\n');
+	return (0);
 }
