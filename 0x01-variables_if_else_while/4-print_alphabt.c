fcb0154913bc716d7d83906fcef0468b76e65d0d
Wright <calebogundiya@gmail.com>
2023-03-20 19:12:55 +0000

    Print Alphabet, no e or q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+#include <ctype.h>
 
 /**
 * main - start the app
 *
-* Return: 1
+* Return: 0
 */
 
 int main(void)
 {
-	
-	return (1);
+	char x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		if (x == 'P' || x == 'E')
+			continue;
+		putchar(tolower(x));
+	}
+	putchar('\n');
+	return (0);
 }
