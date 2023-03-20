12254a65b49820fbde83410cd6d8498980df8224
Wright <calebogundiya@gmail.com>
2023-03-20 19:15:45 +0000

    Print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
+#include <ctype.h>
 
 /**
 * main - start the app
 *
-* Return: 1
+* Return: 0
 */
 
 int main(void)
 {
-	
-	return (1);
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+	putchar('\n');
+	return (0);
 }
