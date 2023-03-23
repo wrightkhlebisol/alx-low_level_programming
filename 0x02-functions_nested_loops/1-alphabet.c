3124d8fab3629c9ec9f63674d961f89ecb66c2d6
Wright <calebogundiya@gmail.com>
2023-03-23 01:54:50 +0000

    Putchar alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include "_putchar.c"
+#include "main.h"
 
 /**
 * main - start the app
 *
-* Return: 1
+* Return: always 0
 */
 
 int main(void)
 {
-	
-	return (1);
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+	return (0);
 }
