10f7429725ea9027ec8630283adc32194ae0e657
Wright <calebogundiya@gmail.com>
2023-03-24 20:57:38 +0000

    Draws straight line

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_numbers - prints numbers from 0 to 9
+*
+* Return: always 0
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_line - prints _ n times
+*/
+
+void print_line(int n)
+{
+	if (n == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+        	{
+                	_putchar('_');
+        	}
+		_putchar('\n');
+	}
+}
