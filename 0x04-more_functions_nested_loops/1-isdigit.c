3d8e3fe3e1c837a1211c05e126bbada5600e0cd2
Wright <calebogundiya@gmail.com>
2023-03-24 18:22:48 +0000

    checks is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* _isdigit - checks if c is upper
+* @c: Character to compare
+*
+* Return: always 0
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
