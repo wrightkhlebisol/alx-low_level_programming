0a410f3c133cd5b72e53f7948bd695eb1cb51fdf
Wright <calebogundiya@gmail.com>
2023-03-24 22:25:56 +0000

    draws square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_square - prints # as square
+* @size: size of square to print
+*/
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		/* Outside loop for rows  */
+		for (i = 0; i < size; i++)
+		{
+			int j;
+
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/test.c
+++ 0x04-more_functions_nested_loops/test.c
@@ -10,37 +10,30 @@
 
 int main(void)
 {
-	print_line(10);	
+	print_square(3);	
 }
 
-void print_line(int n)
+void print_square(int size)
 {
-        if (n <= 0)
-        {
-                _putchar('\n');
-        }
-        else
-        {
-                int i;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
 
-                /* Outside loop for rows  */
-                for (i = 0; i < n; i++)
-                {
-                        int j;
+		/* Outside loop for rows  */
+		for (i = 0; i < size; i++)
+		{
+			int j;
 
-                        for(j = 0; j < n; j++)
-                        {
-                                if(j == i)
-                                {
-                                        _putchar('\\');
-                                }
-                                else
-                                {
-                                        _putchar(' ');
-                                }
-                        }
-                        _putchar('\n');
-                }
-                _putchar('\n');
-        }
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
 }
