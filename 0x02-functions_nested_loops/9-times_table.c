1309d33e962125be6782483630e63da56a4e13c0
Wright <calebogundiya@gmail.com>
2023-03-25 02:22:17 +0000

    Multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,34 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - start the app
+* times_table - print multiplication from 0
 *
-* Return: 1
+* Return: void
 */
 
-int main(void)
+void times_table(void)
 {
-	
-	return (1);
+	int a, b, op;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
+		{
+			op = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (op <= 9)
+			{
+				_putchar(32);
+				_putchar(op + 48);
+			}
+			else
+			{
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 int _putchar(char);
 int print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
