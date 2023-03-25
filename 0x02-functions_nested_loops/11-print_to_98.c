df5d91925b66101b18a1109496e9b42be57ae8ad
Wright <calebogundiya@gmail.com>
2023-03-25 02:43:54 +0000

    print to/from n

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
 * print_to_98 - print from n to 98
@@ -9,9 +9,20 @@
 
 void print_to_98(int n)
 {
-	while(n <= 98)
+	if (n < 98)
 	{
-		_putchar(n);
-		n++;
+		while (n <= 98)
+		{
+			printf("%d", n);
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			n--;
+		}
 	}
 }
