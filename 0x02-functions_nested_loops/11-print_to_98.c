fe63835b3839497264e4e98dfc21cc7be3ce95a7
Wright <calebogundiya@gmail.com>
2023-03-25 02:52:48 +0000

    print to/from n

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,20 +13,18 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			printf("%d\n", n);
-			if (n != 98)
-				putchar(44);
+			printf("%d, ", n);
 			n++;
 		}
+		put('\n');
 	}
 	else
 	{
 		while (n >= 98)
 		{
-			printf("%d\n", n);
-			if (n != 98)
-				putchar(44);
+			printf("%d, ", n);
 			n--;
 		}
+		put('\n');
 	}
 }
