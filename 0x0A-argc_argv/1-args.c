98d29108403b4c55bd0e4a070c1f603f4e3e17f2
Wright <calebogundiya@gmail.com>
2023-04-10 16:02:58 +0100

    Print number of args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,16 @@
 # include <stdio.h>
+# include "main.h"
+# include "_putchar.c"
 
 /**
-* main - Prints its name
+* main - Prints number of passed arguments
 * @argc: Count of arguments passed
 * @argv: Array of argument values
 *
 * Return: integer
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
+	printf("%d\n", argc);
 }
