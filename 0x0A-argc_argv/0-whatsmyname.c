19b4ceea56683f74c28b6bb67b6a3c7444bf8915
Wright <calebogundiya@gmail.com>
2023-04-10 15:41:58 +0100

    Print file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+/**
+* main - Prints its name
+* @argc: Count of arguments passed
+* @argv: Array of argument values
+*
+* Return: integer
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+/**
+* main - Prints its name
+* @argc: Count of arguments passed
+* @argv: Array of argument values
+*
+* Return: integer
+*/
+
+int main(int argc, char *argv[])
+{
+}
