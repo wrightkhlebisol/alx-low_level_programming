8428d6b15a6b87aed1f43aabb5b1e879db666705
Wright <calebogundiya@gmail.com>
2023-04-10 16:12:52 +0100

    Print all arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,20 @@
 # include <stdio.h>
+# include "main.h"
+# include "_putchar.c"
 
 /**
-* main - Prints its name
+* main - Prints passed arguments
 * @argc: Count of arguments passed
 * @argv: Array of argument values
 *
 * Return: integer
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+	return (0);
 }
