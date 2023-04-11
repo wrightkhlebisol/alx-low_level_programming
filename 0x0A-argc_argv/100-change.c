a9117dfe86786dd5056621c5b309c103396ac0e8
Wright <calebogundiya@gmail.com>
2023-04-11 01:19:05 +0100

    Make change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,4 +1,6 @@
-# include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
 /**
 * main - Prints the minimum number of coins to make change for
@@ -8,8 +10,44 @@
 * Return: integer
 */
 
-int main(int argc, char __attribute__((__unused__)) *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
+	int change, input;
+	unsigned int x;
+	char *p;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	input = strtol(argv[1], &p, 10);
+	change = 0;
+
+	if (!*p)
+	{
+		while (input > 1)
+		{
+			for (x = 0; x < sizeof(coins[x]); x++)
+			{
+				if (input >= coins[x])
+				{
+					change += input / coins[x];
+					input %= coins[x];
+				}
+			}
+		}
+		if (input == 1)
+			change++;
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", change);
 	return (0);
 }
