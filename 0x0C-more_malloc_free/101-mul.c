53f28d813739bb446d8d838f95d548fe78fefc48
Wright <calebogundiya@gmail.com>
2023-04-14 04:31:46 +0100

    Multiply c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,18 +2,6 @@
 #include <stdlib.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-        return (write(1, &c, 1));
-}
-
-/**
  * _isdigit - checks if a character is a digit
  * @c: character to check
  *
@@ -21,7 +9,7 @@ int _putchar(char c)
  */
 int _isdigit(int c)
 {
-        return (c >= '0' && c <= '9');
+	return (c >= '0' && c <= '9');
 }
 
 /**
@@ -32,12 +20,12 @@ int _isdigit(int c)
  */
 int _strlen(char *s)
 {
-        int i = 0;
+	int i = 0;
 
-        while (s[i])
-                i++;
+	while (s[i])
+		i++;
 
-        return (i);
+	return (i);
 }
 
 /**
@@ -49,82 +37,106 @@ int _strlen(char *s)
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-        char *ptr;
-        unsigned int i;
+	char *ptr;
+	unsigned int i;
 
-        if (nmemb == 0 || size == 0)
-                return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-        ptr = malloc(nmemb * size);
+	ptr = malloc(nmemb * size);
 
-        if (ptr == NULL)
-                return (NULL);
+	if (ptr == NULL)
+		return (NULL);
 
-        for (i = 0; i < (nmemb * size); i++)
-                ptr[i] = 0;
+	for (i = 0; i < (nmemb * size); i++)
+		ptr[i] = 0;
 
-        return ((void *)ptr);
+	return ((void *)ptr);
 }
 
 /**
- * mul - multiplies two positive numbers
+ * multiply - multiplies two positive numbers
  * @num1: first number to multiply
  * @num2: second number to multiply
- *
- * Return: void
  */
-void mul(char *num1, char *num2)
+void multiply(char *num1, char *num2)
 {
-        int len1, len2, i, j, res;
-        int *result;
+	int len1, len2;
+	int *result;
 
-        len1 = _strlen(num1);
-        len2 = _strlen(num2);
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
 
-        result = _calloc(len1 + len2, sizeof(int));
+	result = _calloc(len1 + len2, sizeof(int));
 
-        if (result == NULL)
-        {
-                _putchar('0');
-                _putchar('\n');
-                exit(98);
-        }
+	if (result == NULL)
+	{
+		_putchar('0');
+		_putchar('\n');
+		exit(98);
+	}
 
-        for (i = len1 - 1; i >= 0; i--)
-        {
-                for (j = len2 - 1; j >= 0; j--)
-                {
-                        res = (num1[i] - '0') * (num2[j] - '0');
-                        result[i + j + 1] += res % 10;
-                        result[i + j] += res / 10;
-                }
-        }
+	calculate_product(num1, num2, len1, len2, result);
 
-        for (i = len1 + len2 - 1; i >= 0; i--)
-        {
-                if (result[i] > 9)
-                {
-                        result[i - 1] += result[i] / 10;
-                        result[i] %= 10;
-                }
-        }
+	print_result(len1, len2, result);
 
-        i = 0;
-        while (i < len1 + len2 && result[i] == 0)
-                i++;
-
-        if (i == len1 + len2)
-        {
-                _putchar('0');
-        }
-        else
-        {
-                while (i < len1 + len2)
-                        _putchar(result[i++] + '0');
-        }
-
-        _putchar('\n');
-
-        free(result);
+	free(result);
 }
 
+/**
+ * calculate_product - calculates the product of two numbers
+ * @num1: first number to multiply
+ * @num2: second number to multiply
+ * @len1: length of num1
+ * @len2: length of num2
+ * @result: array to store the result
+ */
+void calculate_product(char *num1, char *num2, int len1, int len2, int *result)
+{
+	int i, j, res;
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			res = (num1[i] - '0') * (num2[j] - '0');
+			result[i + j + 1] += res % 10;
+			result[i + j] += res / 10;
+		}
+	}
+
+	for (i = len1 + len2 - 1; i >= 0; i--)
+	{
+		if (result[i] > 9)
+		{
+			result[i - 1] += result[i] / 10;
+			result[i] %= 10;
+		}
+	}
+}
+
+/**
+ * print_result - prints the result of the multiplication
+ * @len1: length of num1
+ * @len2: length of num2
+ * @result: array storing the result
+ */
+void print_result(int len1, int len2, int *result)
+{
+	int i = 0;
+
+	while (i < len1 + len2 && result[i] == 0)
+		i++;
+
+	if (i == len1 + len2)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		while (i < len1 + len2)
+			_putchar(result[i++] + '0');
+	}
+
+	_putchar('\n');
+}
