302bf14a437bb87567430d31b7bbc8b3268438da
Wright <calebogundiya@gmail.com>
2023-04-18 00:34:13 +0100

    Struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+* struct dog - Dog struct
+* @name: Dog name
+* @age: Dog age
+* @owner: Dog owner
+*
+* Description: Holds data for a dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
