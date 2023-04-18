76082359e1f674fd43be5a7965d2c4d142bf9b81
Wright <calebogundiya@gmail.com>
2023-04-18 03:22:58 +0100

    Fix float format

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: ");
 		d->name == 0 ? printf("(nil)\n") : printf("%s\n", d->name);
-		printf("Age: %.1f\n", d->age);
+		printf("Age: %f\n", d->age);
 		printf("Owner: ");
 		d->owner == 0 ? printf("(nil)\n") : printf("%s\n", d->owner);
 	}
