636fbeccc469e727977886ce107e5764968e64fc
wrightkhlebisol <calebogundiya@gmail.com>
2023-04-22 02:59:04 +0100

    Main file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,1 +1,43 @@
-#include "function_pointers.h";
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 on success, Error on failure
+ */
+int main(int argc, char **argv)
+{
+	int a, b, result;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = op_func(a, b);
+	printf("%d\n", result);
+
+	return (0);
+}
