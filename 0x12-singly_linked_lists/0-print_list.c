68a0941c75e090f0731010683357eb14f24d4ce3
wrightkhlebisol <calebogundiya@gmail.com>
2023-04-28 00:47:23 +0100

    Print node

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
 * print_list - Prints all elements of list_t list
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef _LINKED_LIST_
 #define _LINKED_LIST_
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
