c1ea8785f5fccdc39078eac18317ebf51fc5c6bb
wrightkhlebisol <calebogundiya@gmail.com>
2023-04-28 02:02:29 +0100

    Frees a list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,5 +10,13 @@
 
 void free_list(list_t *head)
 {
-	free(head);
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
 }
