12daa23c18dbb6fdd2978be15972bc80002e441a
wrightkhlebisol <calebogundiya@gmail.com>
2023-05-27 21:47:50 +0100

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,35 @@
+#include "main.h"
 /**
-* create_file - Creates a file
-* @filename: Name of file to create
-* @text_content: Content to write
-*
-* Return: int - op result
-*/
+ * create_file - Creates a file
+ * @filename: Name of file to create
+ * @text_content: Content to write
+ *
+ * Return: int - op result
+ */
 
 int create_file(const char *filename, char *text_content)
 {
-	return (0);
+	int fd = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	// Open the file with write-only permissions 
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	
+	if (fd == -1)
+		return (-1);
+
+	// Write the text content to the file if it is not NULL
+	if (text_content != NULL) {
+		size_t len = strlen(text_content);
+		ssize_t bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1) {
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,10 @@
 #define _FILE_I_O_
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
