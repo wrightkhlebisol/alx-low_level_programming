0d8493fefc6fe5caa818fa0807c227b74165edab
wrightkhlebisol <calebogundiya@gmail.com>
2023-05-28 00:24:16 +0100

    Print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,8 +1,33 @@
 #include "main.h"
 
 /**
-* print_binary
-* @n
-*/
+ * print_binary - Print binary
+ * @n: number to print in binary
+ */
 void print_binary(unsigned long int n)
-{}
+{
+	unsigned long int mask;
+	int leading_zeros;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	leading_zeros = 1;
+
+	while (mask != 0)
+	{
+		if ((n & mask) != 0)
+		{
+			leading_zeros = 0;
+			printf("1");
+		}
+		else if (!leading_zeros)
+			printf("0");
+
+		mask >>= 1;
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef _BIT_MANI_
 #define _BIT_MANI_
 #include <stddef.h>
+#include <stdio.h>
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
