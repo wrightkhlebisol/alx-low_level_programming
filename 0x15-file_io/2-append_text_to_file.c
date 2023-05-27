916bcff7c5e890036582a0e9d6088e533d142837
wrightkhlebisol <calebogundiya@gmail.com>
2023-05-27 22:03:13 +0100

    Fix imports

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * append_text_to_file - add text_content to file
  * @filename: name of file to append to
@@ -9,6 +10,7 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	FILE *file = NULL;
+	size_t len;
 
 	if (filename == NULL)
 		return (-1);
@@ -23,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	/** Append the text content to the file */
-	size_t len = strlen(text_content);
+	len = strlen(text_content);
 
 	if (fwrite(text_content, sizeof(char), len, file) != len)
 	{
