cfd8d03fa7539378a743a5d751acf6b0270bc5b0
wrightkhlebisol <calebogundiya@gmail.com>
2023-05-27 21:51:33 +0100

    Fix betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,17 +14,20 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	// Open the file with write-only permissions 
+	/** Open the file with write-only permissions */
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	
+
 	if (fd == -1)
 		return (-1);
 
-	// Write the text content to the file if it is not NULL
-	if (text_content != NULL) {
+	/** Write the text content to the file if it is not NULL */
+	if (text_content != NULL)
+	{
 		size_t len = strlen(text_content);
 		ssize_t bytes_written = write(fd, text_content, len);
-		if (bytes_written == -1) {
+
+		if (bytes_written == -1)
+		{
 			close(fd);
 			return (-1);
 		}
