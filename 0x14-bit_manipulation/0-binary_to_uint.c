4aa1bba47ce180373e1b0099b35ddd8429e7f1a4
wrightkhlebisol <calebogundiya@gmail.com>
2023-05-28 00:10:53 +0100

    Fix var declared in loop

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,11 +8,12 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
+	size_t i;
 
 	if (b == NULL)
 		return (0);
 
-	for (size_t i = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] == '0')
 			result = (result << 1) | 0;
