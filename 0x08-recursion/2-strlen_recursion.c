372a34b113f63ed40bda69f06e6c5fabe3253898
wrightkhlebisol <calebogundiya@gmail.com>
2023-06-09 03:59:58 +0100

    count string recurs

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns length of a string 
+ * _strlen_recursion - returns length of a string
  * @s: String to count
  *
  * Return: length of string
