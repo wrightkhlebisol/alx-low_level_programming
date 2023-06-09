ccf44ae79ce09ed29d67db670f83b712a0178af4
wrightkhlebisol <calebogundiya@gmail.com>
2023-06-10 00:34:02 +0100

    print command name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints command name
+ * @argc: number of arguments
+ * @argv: array of command line argument
+ * Return: always 0
+ */
+int main(int __attribute__((unused)) argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
