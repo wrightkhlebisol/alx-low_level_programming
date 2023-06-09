a7efa08048abd1def3be6b413f8e9079acbf6391
wrightkhlebisol <calebogundiya@gmail.com>
2023-06-10 00:51:23 +0100

    add attribute

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * @argv: array of command line argument
  * Return: always 0
  */
-int main(int __attribute__((unused)) argc, char **argv)
+int main(__attribute__((unused)) int argc, char **argv)
 {
 	printf("%s\n", argv[0]);
 	return (0);
