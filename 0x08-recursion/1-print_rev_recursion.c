d777ac077aec05397e775ab59fa6c3506b657ac6
wrightkhlebisol <calebogundiya@gmail.com>
2023-06-09 03:44:00 +0100

    recursively reverse a string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+/**
+ * _print_rev_recursion - Print reverse of given string
+ * @s: String to reverse
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
