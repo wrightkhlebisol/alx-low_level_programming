dd1ff2da53f08b96bf2c9251772ef22715867aa7
wrightkhlebisol <calebogundiya@gmail.com>
2023-06-09 10:39:08 +0100

    recursive power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,1 +1,18 @@
 #include "main.h"
+
+/**
+ * _pow_recursion - calculate power of given int
+ * @x: integer to get power of
+ * @y: power to raise
+ *
+ * Return: power of x to y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
