660ec34f98e055b35f3bb6151d2f8ec85fda2e2a
wrightkhlebisol <calebogundiya@gmail.com>
2023-06-10 00:36:34 +0100

    add args

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Add positive numbers to infinity
+ * @argc: number of arguments
+ * @argv: array of command line argument
+ * Return: always 0
+ */
+int main(int argc, char __attribute__((unused)) **argv)
+{
+	int sum = 0, i = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	while (i < argc)
+	{
+		if (atoi(argv[i + 1]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i + 1]);
+		i++;
+	}
+
+	return (sum);
+}
