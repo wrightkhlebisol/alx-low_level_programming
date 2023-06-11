b14387b93f05cba84a5b066bd1b02e0b6e3ae4df
wrightkhlebisol <calebogundiya@gmail.com>
2023-06-11 22:48:29 +0100

    modified static libs

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,1 +1,14 @@
 #include "main.h"
+
+/**
+ * _sqrt_recursion - Recursive square root
+ * @n: integer to root
+ * Return: square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n <= 0)
+		return (-1);
+
+	return (_sqrt_recursion(n));
+}
