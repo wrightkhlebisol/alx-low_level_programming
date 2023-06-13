e2752b8b2ec7a4c3ca5095717ad949982b7909a5
wrightkhlebisol <calebogundiya@gmail.com>
2023-06-13 20:19:26 +0100

    string dup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,4 +7,25 @@
  * Return: Pointer or NULL
  */
 char *_strdup(char *str)
-{}
+{
+	char *ch;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	ch = malloc(sizeof(str));
+
+	if (ch == NULL)
+		return (NULL);
+
+	while (*str != '\0')
+	{
+		ch[i] = *str;
+		str++;
+		i++;
+	}
+
+	ch[i] = '\0';
+	return (ch);
+}
