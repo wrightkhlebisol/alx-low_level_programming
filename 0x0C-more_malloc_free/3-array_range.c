4f4d765e7df5e1872319592b27a0fb46a6fc1144
wrightkhlebisol <calebogundiya@gmail.com>
2023-06-15 23:06:29 +0100

    create int array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,10 +1,24 @@
 #include "main.h"
 /**
- * array_range -
- * @min:
- * @max:
+ * array_range - Creates an array if integers
+ * @min: Minimum integer
+ * @max: Maximum integer
  *
- * Return:
+ * Return: NULL or Pointer
  */
 int *array_range(int min, int max)
-{}
+{
+	int *numarray, i = 0;
+
+	if (min > max)
+		return (NULL);
+
+	numarray = malloc(sizeof(int) * (max - min) + 1);
+	if (numarray == NULL)
+		return (NULL);
+
+	while (min <= max)
+		numarray[i++] = min++;
+
+	return (numarray);
+}
