6f432e658264ece54a9161c302bceaa57c12e4f3
wrightkhlebisol <calebogundiya@gmail.com>
2023-06-15 23:42:37 +0100

    concatenate n string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,11 +1,37 @@
-#include "main.c"
+#include "main.h"
+int countchar(char *str)
+{
+	if (*str != '\0')
+		return (0);
+
+	return (1 + countchar(str++));
+}
+
 /**
- * string_nconcat -
- * @s1:
- * @s2:
- * @n:
+ * string_nconcat - Concatenates 2 strings
+ * @s1: First string
+ * @s2: Second string
+ * @n: Number of bytes to add
  *
  * Return: NULL or Pointer
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
-{}
+{
+	char *concatstring;
+	unsigned int i = 0, j = 0;
+
+	concatstring = malloc(countchar(s1) + n);
+	if (concatstring == NULL)
+		return (NULL);
+
+	while(*s1 != '\0')
+		concatstring[i++] = *s1++;
+
+	while(j < n)
+	{
+		concatstring[i++] = *s2++;
+		j++;
+	}
+
+	return (concatstring);
+}
--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _realloc - 
- * @ptr:
- * @old_size:
- * @new_size:
+ * _realloc - realloc using malloc and free 
+ * @ptr: Pointer to previously allocated memory
+ * @old_size: Old memory size
+ * @new_size: New memory size
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {}
