e4ee731542e06a993348c2e7e1aa64190c7391a1
wrightkhlebisol <calebogundiya@gmail.com>
2023-06-20 20:58:35 +0100

    dog header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - dog details
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
