9dfc70062651e83719a2a33d61b67eac49f859d5
wrightkhlebisol <calebogundiya@gmail.com>
2023-06-22 00:07:21 +0100

    searches for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - Searches for an integer
+ * @array: array of int
+ * @size: size of array
+ * @cmp: Comparison function
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0 || array == NULL)
+		return (-1);
+
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
