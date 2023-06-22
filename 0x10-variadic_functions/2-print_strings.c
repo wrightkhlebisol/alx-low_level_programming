d88a4fd181f821892f5abc21a4fba2ce331677a6
wrightkhlebisol <calebogundiya@gmail.com>
2023-06-22 23:12:19 +0100

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -5,4 +5,26 @@
  * @n: Number of variables
  */
 void print_strings(const char *separator, const unsigned int n, ...)
-{}
+{
+	va_list ap;
+	unsigned int i = 0;
+
+	if (n > 0)
+	{
+		va_start(ap, n);
+		while (i < n)
+		{
+			printf("%s", va_arg(ap, char*));
+			i++;
+			if (separator == NULL)
+				continue;
+			if (i == n)
+			{
+				printf("\n");
+				break;
+			}
+			printf("%s", separator);
+		}
+		va_end(ap);
+	}
+}
