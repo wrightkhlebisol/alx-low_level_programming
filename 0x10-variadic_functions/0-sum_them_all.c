0a8156bcca75974701893979bab79d559764f644
wrightkhlebisol <calebogundiya@gmail.com>
2023-06-22 18:46:49 +0100

    Print numbers

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -19,5 +19,6 @@ int sum_them_all(const unsigned int n, ...)
 		sum += va_arg(ap, int);
 		i++;
 	}
+	va_end(ap);
 	return (sum);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,7 +2,31 @@
 /**
  * print_numbers - Print numbers
  * @separator: separator
- * @n:
+ * @n: Number of var args
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
-{}
+{
+	va_list ap;
+	unsigned int i = 0;
+
+
+	if (n > 0)
+	{
+		va_start(ap, n);
+		while (i < n)
+		{
+			printf("%d", va_arg(ap, int));
+			i++;
+			if (i == n)
+			{
+				printf("\n");
+				break;
+			}
+
+			if (separator == NULL)
+				continue;
+			printf("%s", separator);
+		}
+		va_end(ap);
+	}
+}
