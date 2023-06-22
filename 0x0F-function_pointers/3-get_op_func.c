164f5825ecd28c6fdaa42790dc6e042c6b92ef9c
wrightkhlebisol <calebogundiya@gmail.com>
2023-06-22 01:54:39 +0100

    Include required declarations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,3 +1,4 @@
+#include "3-calc.h"
 /**
  * get_op_func - Get operation function
  * @s: opeation type
@@ -5,5 +6,14 @@
  */
 int (*get_op_func(char __attribute__((unused)) *s))(int, int)
 {
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 	return (0);
 }
