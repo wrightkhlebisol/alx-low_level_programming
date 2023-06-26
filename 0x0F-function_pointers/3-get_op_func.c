893fb1e1529354b4cda095efa2b98abc30d77afa
wrightkhlebisol <calebogundiya@gmail.com>
2023-06-26 10:25:11 +0100

    Implement get op function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,8 +19,8 @@ int (*get_op_func(char *s))(int, int)
 	while (ops[i].op != NULL)
 	{
 		if (*(ops[i]).op == *s)
-			return *(ops[i]).f;
-		i++; 
+			return (*(ops[i]).f);
+		i++;
 	}
 	return (0);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,17 +10,16 @@ int main(int argc, char **argv)
 	int opd_1, opd_2;
 	char *opert;
 
-	printf("argc: %d\n", argc);
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	opd_1 = atoi(argv[1]); 
+	opd_1 = atoi(argv[1]);
 	opd_2 = atoi(argv[3]);
 	opert = argv[2];
-	
+
 	printf("%d\n", get_op_func(opert)(opd_1, opd_2));
 	return (0);
 }
