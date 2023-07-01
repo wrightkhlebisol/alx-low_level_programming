f9a060b1b90bce734407d6819aa4a3cdbe762c9b
wrightkhlebisol <calebogundiya@gmail.com>
2023-07-01 19:50:31 +0100

    return zero on NULL

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@ size_t print_list(const list_t *h)
 	size_t node_num = 0;
 
 	if (h == NULL)
-		exit(1);
+		return (0);
 
 	while (h != NULL)
 	{
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -45,11 +45,12 @@ list_t *add_node_end(list_t **head, const char *str)
 		while (*head != NULL)
 		{
 			list_t *temp = *head;
+
 			*head = (*head)->next;
 			if (*head == NULL)
 			{
 				new_node->next = NULL;
-				(*head)->next = new_node;
+				(*temp)->next = new_node;
 				break;
 			}
 		}
