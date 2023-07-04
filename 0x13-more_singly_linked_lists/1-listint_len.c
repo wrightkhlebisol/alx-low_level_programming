301cb22b4af17d8fe1e9864f6373c97461d2d26b
wrightkhlebisol <calebogundiya@gmail.com>
2023-07-04 16:52:15 +0100

    count number of elements

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,21 @@
 #include "lists.h"
+/**
+ * listint_len - Count number of elements in a linked list
+ * @h: List head
+ * Return: Unsigned int or 0
+ */
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
