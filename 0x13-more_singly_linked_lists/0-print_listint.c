a26ee0d7480a2049fd58be39f4536d8694247889
wrightkhlebisol <calebogundiya@gmail.com>
2023-07-04 16:36:05 +0100

    Print list members

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_listint - Prints elements of a list
+ * @h: Struct list
+ * Return: Unsigned int
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (1);
+
+	while (h != NULL)
+	{
+		count++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}
