19a81cc4b8b25e070393df0ffdecfabadff7c6c1
wrightkhlebisol <calebogundiya@gmail.com>
2023-07-05 23:54:41 +0100

    free pointer to first node

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,1 +1,18 @@
 #include "lists.h"
+/**
+ * free_listint2 - Free a List
+ * @head: Pointer to head of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = NULL;
+
+	if (*head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		temp = *head;
+		free(*head);
+		*head = temp->next;
+	}
+}
