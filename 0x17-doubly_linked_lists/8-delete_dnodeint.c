0c2d31744c95c7800342275bac240e7f5a888dec
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-21 00:27:43 +0100

    delete node at index

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Add node at index
+ * @h: Head of node
+ * @index: Index to add node at
+ * @n: Node value
+ *
+ * Return: Pointer to new_node
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head, *prev = NULL;
+	unsigned int node_index = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	while (temp != NULL)
+	{
+		if (node_index == index)
+		{
+			if (node_index == 0 && *head != NULL)
+			{
+				*head = temp->next;
+				/** temp->next->prev = NULL; */
+				free(temp);
+				return (1);
+			}
+			else
+			{
+				prev->next = temp->next;
+				temp->next->prev = prev;
+				free(temp);
+				return (1);
+
+			}
+		}
+		node_index++;
+		prev = temp;
+		temp = temp->next;
+	}
+	return (-1);
+}
