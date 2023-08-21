1cca4211ab406a42694414c7dc321652a26ea455
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-21 22:53:59 +0100

    creating library functions

---
+++ 0x18-dynamic_libraries/clibs/cops.c
@@ -0,0 +1,26 @@
+#include "cops.h"
+
+int add(a, b)
+{
+	return (a + b);
+}
+
+int sub(a, b)
+{
+	return (a - b);
+}
+
+int mul(a, b)
+{
+	return (a * b);
+}
+
+int div(a, b)
+{
+	return (a / b);
+}
+
+int mod(a, b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/clibs/cops.h
@@ -0,0 +1,8 @@
+#ifndef _COPS_
+#define _COPS_
+int add(a, b);
+int sub(a, b);
+int mul(a, b);
+int div(a, b);
+int mod(a, b);
+#endif /** _COPS_  */
