f38e85c5a32ce707c6b21233979378cec5d99a1b
wrightkhlebisol <calebogundiya@gmail.com>
2023-09-09 01:30:59 +0100

    fix variable length c99 error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,11 +8,10 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_t = malloc(sizeof(hash_table_t));
+	hash_node_t **new_array = malloc(size);
 
 	if (new_hash_t == NULL)
 		return (NULL);
-
-	hash_node_t *new_array[size];
 
 	new_hash_t->size = size;
 	new_hash_t->array = new_array;
