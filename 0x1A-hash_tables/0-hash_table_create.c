8250758c4a99a5cb49f75bc6d82dd52e93f2a302
wrightkhlebisol <calebogundiya@gmail.com>
2023-09-09 01:37:03 +0100

    malloc by size and type

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_t = malloc(sizeof(hash_table_t));
-	hash_node_t **new_array = malloc(size);
+	hash_node_t **new_array = malloc(size * sizeof(unsigned long int));
 
 	if (new_hash_t == NULL)
 		return (NULL);
