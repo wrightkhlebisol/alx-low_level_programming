8eae697983d3d4873057141ff25cdebc0d6e90f5
wrightkhlebisol <calebogundiya@gmail.com>
2023-09-10 10:46:18 +0100

    allow empty values

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node_t, **ht_arr;
 	int key_in;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (ht == NULL || key == NULL)
 		return (0);
 
 	new_node_t = malloc(sizeof(hash_node_t));
