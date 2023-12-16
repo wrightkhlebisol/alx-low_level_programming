fc2cd387aa8b679c4ce92a11c3d14d844ed21850
wrightkhlebisol <calebogundiya@gmail.com>
2023-12-16 20:24:25 +0000

    fix betty long var

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -11,7 +11,7 @@
 int jump_search(int *array, size_t size, int value)
 {
 	size_t i = 0, start = 0, end = 0;
-	int jump_step = sqrt(size);
+	int step = sqrt(size);
 
 	if (array == NULL)
 		return (-1);
@@ -21,20 +21,20 @@ int jump_search(int *array, size_t size, int value)
 	{
 		if (array[i] >= value)
 		{
-			start = i - jump_step;
+			start = i - step;
 			end = i;
 			break;
 		}
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		i += jump_step;
+		i += step;
 	}
 
-	printf("Value found between indexes [%ld] and [%ld]\n", i - jump_step, i);
+	printf("Value found between indexes [%ld] and [%ld]\n", i - step, i);
 
 	while (start <= end)
 	{
 		if (start == end)
-			printf("Value checked array[%ld] = [%d]\n", i - jump_step, array[i - jump_step]);
+			printf("Value checked array[%ld] = [%d]\n", i - step, array[i - step]);
 		else
 			printf("Value checked array[%ld] = [%d]\n", start, array[start]);
 		if (array[start] == value)
